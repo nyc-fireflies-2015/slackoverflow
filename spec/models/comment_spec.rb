e4f384128dcd1eff4664281ee5989d89ed69a4d3
GLNRO <lg@glnro.com>
2015-09-11 17:48:47 -0400

    Remove byebug from testing

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 describe Comment do
+  it 'should be valid with a body and commenter_id' do
+    comment = Comment.create(body:"test", commenter_id: 5)
+    comment.valid?
+    expect(comment).to be_valid
+  end
 
+  it 'should be invalid without a body' do
+    comment = Comment.create(commenter_id: 5)
+    comment.valid?
+    expect(comment).to be_valid
+  end
 end
