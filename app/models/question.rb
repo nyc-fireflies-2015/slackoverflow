92906ba55d99ba6cb257bf7ab4401ea37a19e483
emjr <emjrdev@gmail.com>
2015-09-11 16:27:25 -0400

    create spec/models & question_spec

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :author, class_name: :User, foreign_key: :author_id
-
+  
   has_many :comments, as: :commentable
   has_many :taggings
   has_many :tags, through: :taggings
