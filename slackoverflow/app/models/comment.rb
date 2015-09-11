561289036670951c82068c7377f8566be90a9cbc
GLNRO <lg@glnro.com>
2015-09-11 16:01:04 -0400

    Fix polymorphic association in comments model

--- slackoverflow/app/models/comment.rb
+++ slackoverflow/app/models/comment.rb
@@ -1,8 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :commenter, class_name: :User, foreign_key: :commenter_id
-  belongs_to :question
-  has_many :threaded_comments, class_name: :Comment, foreign_key: :parent_comment_id
-  belongs_to :parent_comment, class_name: :Comment
+  has_many :comments, as: :commentable
   has_many :votes, as: :voteable
 end
