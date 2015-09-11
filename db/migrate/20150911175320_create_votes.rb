83a2798ad5b2b419d9f7be8cd0dbce335f4cccfd
GLNRO <lg@glnro.com>
2015-09-11 14:32:49 -0400

    Write votes migration

--- db/migrate/20150911175320_create_votes.rb
+++ db/migrate/20150911175320_create_votes.rb
@@ -1,8 +1,13 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-
+      t.integer   :value, null: false
+      t.integer   :voter_id, null: false
+      t.string    :voteable_type
+      t.integer   :voteable_id
       t.timestamps null: false
     end
+
+    add_index :votes, :voteable_id
   end
 end
