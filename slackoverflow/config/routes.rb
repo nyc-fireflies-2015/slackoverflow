95bddb50908b73be240224ca106d4df6d0e07258
GLNRO <lg@glnro.com>
2015-09-11 16:35:00 -0400

    Create auth routes

--- slackoverflow/config/routes.rb
+++ slackoverflow/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'questons#index'
 
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  get 'login' => 'sessions#new'
+  get 'signup' => 'users#new'
+  post 'login' => 'sessons#create'
+  post 'singup' => 'users#create'
+  get 'logout' => 'sessions#destroy'
 
   resources :users
   resources :questions
