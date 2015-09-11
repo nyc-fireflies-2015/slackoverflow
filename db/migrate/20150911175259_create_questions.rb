f0095df4d95b8ffeb153eeaee5a6fa0f7f3cef9f
GLNRO <lg@glnro.com>
2015-09-11 14:15:00 -0400

    Write question migration

--- db/migrate/20150911175259_create_questions.rb
+++ db/migrate/20150911175259_create_questions.rb
@@ -1,7 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-
+      t.string  :author_id, null: false
+      t.string  :title, null: false
+      t.string  :body, null: false
       t.timestamps null: false
     end
   end
