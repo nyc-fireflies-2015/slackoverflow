a17f825f5a76a3042c32660471a90fd5442a56a8
Ramon Ramos III <email": null>
2015-09-11 18:30:21 -0400

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to Slack Overflow"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  private
+
+    def user_params
+      params.required(:user).permit(:username, :password)
+    end
+
 end
