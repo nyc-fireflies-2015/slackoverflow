2e1c02a3fcc389fc62674f306404293e0a66f33c
GLNRO <lg@glnro.com>
2015-09-11 14:57:20 -0400

    Write question associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,8 @@
 class Question < ActiveRecord::Base
+  belongs_to :author, class_name: :User, foreign_key: :author_id
+
+  has_many :comments, as: :commentable
+
+  has_many :tags, through: :question_tags
+
 end
