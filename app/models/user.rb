7b9f4520fbfdd4a5feae8678ab9eeab15ece023b
Ramon Ramos III <email": null>
2015-09-11 17:07:32 -0400

    Add length restrictions to user and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   has_many :questions, foreign_key: :author_id
   has_many :comments, foreign_key: :commenter_id
 
-  validates :username, uniqueness: true, presence: true
-  validates :password_digest, presence: true
+  validates :username, uniqueness: true, presence: true, length: { in: 3..12 }
+  validates :password_digest, presence: true, length: { in: 3..20 }
 end
