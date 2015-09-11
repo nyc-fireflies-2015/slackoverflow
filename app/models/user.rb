8225479da7698e7ba9c116906b2d990b01f25164
Ramon Ramos III <email": null>
2015-09-11 17:32:45 -0400

    Remove length restrictions on password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   has_many :comments, foreign_key: :commenter_id
 
   validates :username, uniqueness: true, presence: true, length: { in: 3..12 }
-  validates :password_digest, presence: true, length: { in: 3..20 }
+  validates :password_digest, presence: true
 end
