6602f5a907df07b30094cbb4c1898edaad10df9b
Ramon Ramos III <email": null>
2015-09-11 16:45:32 -0400

    Add User validations

--- slackoverflow/app/models/user.rb
+++ slackoverflow/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :questions, foreign_key: :author_id
-
   has_many :comments, foreign_key: :commenter_id
+
+  validates :username, uniqueness: true, presence: true
+  validates :password_digest, presence: true
 end
