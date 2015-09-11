096bb9506a71273a2f82bd355e4857c206ecf7b3
GLNRO <lg@glnro.com>
2015-09-11 14:27:14 -0400

    Write comments migration

--- db/migrate/20150911175243_create_comments.rb
+++ db/migrate/20150911175243_create_comments.rb
@@ -1,8 +1,17 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :body, null: false
+      t.integer :commenter_id, null: false
+      t.integer :question_id, null: false
+      t.integer :parent_comment_id
+      t.boolean :is_answer, default: false
+      t.string :commentable_type
+      t.integer :commentable_id
 
       t.timestamps null: false
     end
+
+    add_index :comments, :commentable_id
   end
 end
