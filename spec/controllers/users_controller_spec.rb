15d49ba0816ae8a0fd7b2a710097905d2542ee12
Ramon Ramos III <email": null>
2015-09-11 19:11:00 -0400

    Update user controller spec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,9 +3,33 @@ require 'rails_helper'
 RSpec.describe UsersController, :type => :controller do
 
   describe 'GET #new' do
-    context '' do
-      it "renders"
+    it "renders the :new template" do
     end
   end
 
+  describe 'GET #show' do
+    it "assigns the requested user to @user" do
+    end
+
+    it "renders the :show template" do
+    end
+  end
+
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it "saves the new user in the database" do
+      end
+
+      it "redirects to users#show" do
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new contact in the database" do
+      end
+
+      it "re-renders the :new template" do
+      end
+    end
+  end
 end
