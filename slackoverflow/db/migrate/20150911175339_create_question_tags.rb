4393dc35d6664c38cd07100c9c5fbfbf7fb6c7e0
GLNRO <lg@glnro.com>
2015-09-11 14:17:47 -0400

    Write join table migration for questions and tags

--- slackoverflow/db/migrate/20150911175339_create_question_tags.rb
+++ slackoverflow/db/migrate/20150911175339_create_question_tags.rb
@@ -1,6 +1,9 @@
 class CreateQuestionTags < ActiveRecord::Migration
   def change
     create_table :question_tags do |t|
+      t.integer :question_id, null: false
+      t.integer :tag_id, null: false
+      t.timestamps, null: false
     end
   end
 end
