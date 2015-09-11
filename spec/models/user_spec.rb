3f0fb4b21b65b2a773c4c1478277135d2da2f8e2
Ramon Ramos III <email": null>
2015-09-11 17:57:57 -0400

    Modify user model spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 describe User do
   context "validations" do
 
-    it "validates presence of username" do
-      user = User.create(username: "john", password: "john")
-      expect(user).to be_valid
-    end
-
-    it "validates presence of password" do
+    it "validates presence of username and password" do
       user = User.create(username: "john", password: "john")
       expect(user).to be_valid
     end
@@ -18,5 +13,16 @@ describe User do
       user2 = User.create(username: "mary", password: "foo")
       expect(user2).to be_invalid
     end
+
+    it "doesn't allow a username smaller than 2 characters" do
+      user = User.create(username: "jo", password: "john")
+      expect(user).to be_invalid
+    end
+
+    it "doesn't allow a username larger than 12 characters" do
+      user = User.create(username: "johnnyappleseed", password: "john")
+      expect(user).to be_invalid
+    end
+
   end
 end
