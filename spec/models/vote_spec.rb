174bdcb5daeb123c80b7a91ae6bce6bcb8afb246
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-12 11:58:44 -0700

    Add vote model spec WIP

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Vote do
+
+  context 'validations' do
+
+    it 'validates presence of a voter making the vote'
+
+    it 'validates presence of a voteable_id'
+
+  end
+
+end
