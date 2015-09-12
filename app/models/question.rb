5d2df40462a2e77175f1838b03c9137a295b532e
emjr <emjrdev@gmail.com>
2015-09-12 10:53:15 -0400

    add basic tagging

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,15 @@ class Question < ActiveRecord::Base
   has_many :votes, as: :voteable
 
   validates :title, :body, :author_id, presence: true
+  
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
+
 end
