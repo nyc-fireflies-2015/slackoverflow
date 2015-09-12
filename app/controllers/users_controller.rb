5a203afceb3211122df6270a375a9ed4dc989ab0
GLNRO <lg@glnro.com>
2015-09-11 23:10:29 -0400

    Edit create route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
       flash[:success] = "Welcome to Slack Overflow"
-      redirect_to user_path(@user)
+      redirect_to user_path(user)
     else
       render 'new'
     end
   end
 
-  def new
-    @user = User.new
-  end
 
   def show
     @user = User.find(params[:id])
