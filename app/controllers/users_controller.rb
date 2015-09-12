e57001a225914d3ccf0671af9f519ef550dfb186
GLNRO <lg@glnro.com>
2015-09-11 22:11:23 -0400

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      session[:user_id] = @user.id
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
       flash[:success] = "Welcome to Slack Overflow"
       redirect_to user_path(@user)
     else
@@ -13,6 +12,7 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def show
