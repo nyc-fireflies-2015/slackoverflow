99fe8f1038266bc5be0974bf271e6517074923ea
GLNRO <lg@glnro.com>
2015-09-11 20:15:55 -0400

    Add strong params to comment controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,1 +1,26 @@
-comments_controller_spec.rb
+class CommentsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def comments_params
+    params.require(:comment).permit(:body, :commenter_id, :is_answer)
+  end
+
+end
