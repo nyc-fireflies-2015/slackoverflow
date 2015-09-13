c0e8789acd48bd0812231c280edea15813fcd2a4
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-13 08:14:29 -0700

    Add working ajax call to server

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
   $(".upvote-question").on('click', function(e) {
-    console.log("HI")
+    var clickTarget = e.target;
+    // debugger
+    $.ajax({
+      url: "/questions/" + clickTarget.name + "/vote",
+      method: "post"
+    }).done(function(response) {
+      console.log("yay, here's the response", response)
+    }).fail(function(err) {
+      console.log(':(', err)
+    })
+
   });
 });
