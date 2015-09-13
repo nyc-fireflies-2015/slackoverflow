8b44352b9239e4e2717da6154de01703d239648d
arjis02 <rj.ramos@gmail.com>
2015-09-12 22:48:47 -0400

    Update seed file to create more questions, comments, and answer

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,14 @@ user_1 = User.create(username:"larry",password:"1234")
 user_2 = User.create(username:"curly",password:"1234")
 user_3 = User.create(username:"moe",password:"1234")
 
-question = user_1.questions.create(title:"test",body:"test")
+10.times do
+	question = user_1.questions.create(title: Faker::Hacker.noun, body: Faker::Hacker.say_something_smart)
+	answer1 = question.comments.create(body: Faker::Hacker.say_something_smart, commenter_id: 1, is_answer: true)
+	answer2 = question.comments.create(body: Faker::Hacker.say_something_smart, commenter_id: 3, is_answer: true)
+	3.times do
+		comment = question.comments.create(body: Faker::Hacker.say_something_smart,commenter_id: 2)
+	end
+end
 
-comment_1 = question.comments.create(body:"test",commenter_id: 2)
-comment_2 = comment_1.comments.create(body:"test",commenter_id: 3)
+# comment_1 = question.comments.create(body:"test",commenter_id: 2)
+# comment_2 = comment_1.comments.create(body:"test",commenter_id: 3)
